a699ec566c965a52941ccb91543d856158195451
TheoElia <theophylusnhutiphapha@gmail.com>
2019-12-04 21:04:25 +0000

    P-mart begins...on git

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django_mysql.models import ListTextField
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    # add additional fields in here
+    email = models.EmailField(null=True,blank=True)
+    is_staff = models.BooleanField(default=True)
+
+    
